fe65d0951a5c84a7b36c94e619e2123ee14a5a1b
Hammad Afzal <hammad.afzal.code@gmail.com>
2024-04-19 08:16:14 -0700

    all sections done && responsiveness

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,23 @@ import {
 } from "react-icons/ai";
 import Image from "next/image";
 import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
+import { useState } from "react";
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Hammad Afzal</title>
       </Head>
-      <main className="bg-white px-10 ">
+      <main className="bg-white px-10 md:px-20 lg:px-40 ">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">Developed by Hammad Afzal</h1>
@@ -32,11 +42,11 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 ">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Hammad Afzal
             </h2>
-            <h3 className="text-2xl py-2">Fullstack Developer.</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <h3 className="text-2xl py-2 md:text-3xl">Fullstack Developer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto">
               Full-Stack Developer specializing in the MERN stack. Explore my
               portfolio to see my blend of design and technology in action.
             </p>
@@ -46,8 +56,149 @@ export default function Home() {
             <AiFillLinkedin />
             <AiFillGithub />
           </div>
-          <div className="mx-auto relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <div className="mx-auto relative bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={deved} layout="fill" objectFit="cover " />
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Service I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Specializing in <span className="text-teal-500">MERN</span>{" "}
+              projects, I offer{" "}
+              <span className="text-teal-500">full-stack </span> development
+              services that transform ideas into robust, scalable web
+              applications. Let's build the future together!
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer wide range of services, including design & development and
+              post development services also.{" "}
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={design}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image src={code} width={100} height={100} className="mx-auto" />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">Providing coding solutions</p>
+              <h4 className="py-4 text-teal-600">Coding Frameworks I use</h4>
+              <p className="text-gray-800 py-1">React js</p>
+              <p className="text-gray-800 py-1">Node js</p>
+              <p className="text-gray-800 py-1">Express js</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={consulting}
+                width={100}
+                height={100}
+                className="mx-auto"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Project consultancy
+              </h3>
+              <p className="py-2">
+                Providing consultancy for your next big project
+              </p>
+              <h4 className="py-4 text-teal-600">
+                Consultancy Packages we have
+              </h4>
+              <p className="text-gray-800 py-1">Basic</p>
+              <p className="text-gray-800 py-1">Pro</p>
+              <p className="text-gray-800 py-1">Pro+</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Specializing in <span className="text-teal-500">MERN</span>{" "}
+              projects, I offer{" "}
+              <span className="text-teal-500">full-stack </span> development
+              services that transform ideas into robust, scalable web
+              applications. Let's build the future together!
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer wide range of services, including design & development and
+              post development services also.{" "}
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            {" "}
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web1}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web2}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web3}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web4}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web5}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={web6}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
           </div>
         </section>
       </main>
